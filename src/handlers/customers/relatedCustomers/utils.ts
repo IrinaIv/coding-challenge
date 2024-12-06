3b6894a3bc022e045f32c0ada68d133739cfb5f8
Irina Ivashkova <15821402+IrinaIv@users.noreply.github.com>
2024-12-06 23:14:34 +0100

    Improvements (#5)

--- src/handlers/customers/relatedCustomers/utils.ts
+++ src/handlers/customers/relatedCustomers/utils.ts
@@ -13,6 +13,18 @@ export const getP2PCustomerTransactions = (allTransactions: Transaction[], custo
   });
 };
 
+export const getSameDeviceTransactions = (allTransactions: Transaction[], customerId: number): Transaction[] => {
+  const devices = new Set(
+    allTransactions
+      .filter((transaction: Transaction) => transaction.customerId === customerId && transaction.metadata.deviceId)
+      .map((transaction: Transaction) => transaction.metadata.deviceId)
+  );
+
+  return allTransactions.filter((transaction: Transaction) => {
+    return devices.has(transaction.metadata.deviceId) && transaction.customerId !== customerId;
+  });
+};
+
 export const retrieveRelatedTransactionData = (transactions: Transaction[]): RelatedTransactionData[] => {
   return transactions
     .map((transaction: Transaction) => {
@@ -26,20 +38,17 @@ export const retrieveRelatedTransactionData = (transactions: Transaction[]): Rel
     .filter(Boolean) as RelatedTransactionData[];
 };
 
-export const getSameDeviceTransactions = (allTransactions: Transaction[], customerId: number): Transaction[] => {
-  const devices = new Set(
-    allTransactions
-      .filter((transaction: Transaction) => transaction.customerId === customerId && transaction.metadata.deviceId)
-      .map((transaction: Transaction) => transaction.metadata.deviceId)
-  );
-
-  return allTransactions.filter((transaction: Transaction) => devices.has(transaction.metadata.deviceId));
+export const retrieveSameDeviceTransactionData = (transactions: Transaction[]): RelatedCustomerResponse[] => {
+  return transactions
+    .map((transaction: Transaction) => ({
+      relatedCustomerId: transaction.customerId,
+      relationType: 'DEVICE',
+    }));
 };
 
-export const createRelatedCustomersList = (
+const createRelatedCustomersList = (
   allTransactionsMap: Map<number, Transaction>,
   relatedTransactions: RelatedTransactionData[],
-  relationType?: 'DEVICE',
 ): RelatedCustomerResponse[] => {
   const result = [];
 
@@ -48,7 +57,7 @@ export const createRelatedCustomersList = (
     if (transaction) {
       result.push({
         relatedCustomerId: transaction.customerId,
-        relationType: relationType ?? relatedTransaction.relationType,
+        relationType: relatedTransaction.relationType,
       });
     }
   }
@@ -56,10 +65,21 @@ export const createRelatedCustomersList = (
   return result;
 };
 
+const getUniqueRelatedCustomersList = (relatedCustomers: RelatedCustomerResponse[]): RelatedCustomerResponse[] => {
+  const uniqueMap: Map<string, RelatedCustomerResponse> = new Map();
+
+  relatedCustomers.forEach(relatedCustomer => {
+    const key = `${relatedCustomer.relatedCustomerId}_${relatedCustomer.relationType}`;
+    uniqueMap.set(key, relatedCustomer);
+  });
+
+  return Array.from(uniqueMap.values());
+};
+
 export const createRelatedCustomersResponse = (
   allTransactions: Transaction[],
   relatedTransactions: RelatedTransactionData[],
-  sameDevicesTransactions: RelatedTransactionData[],
+  sameDevicesTransactions: RelatedCustomerResponse[],
 ): RelatedCustomersResponse => {
   const allTransactionsMap = new Map();
 
@@ -70,7 +90,7 @@ export const createRelatedCustomersResponse = (
   return {
     relatedCustomers: [
       ...createRelatedCustomersList(allTransactionsMap, relatedTransactions),
-      ...createRelatedCustomersList(allTransactionsMap, sameDevicesTransactions, 'DEVICE'),
+      ...getUniqueRelatedCustomersList(sameDevicesTransactions),
     ],
   };
 };
